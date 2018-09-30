4a195068e8e4e12a2677355a5ea562cc67e96114
Josué Cárdenas <rodrigo.cardns@gmail.com>
2018-09-30 00:43:00 -0500

    Añadiendo configuraciones para los modelos.

---
+++ Primera Iteración/Proyecto/biosip/src/main/java/mx/unam/is20191/dao/CategoriaDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.unam.is20191.dao;
+
+import mx.unam.is20191.models.Categoria;
+
+/**
+ *
+ * @author jrcvd
+ */
+
+public class CategoriaDao extends AbstractDao<Integer,Categoria>{
+}
