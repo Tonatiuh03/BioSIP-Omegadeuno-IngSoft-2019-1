a2894393a90be84a9caef7e34790df6d50b7ceb9
Josué Cárdenas <rodrigo.cardns@gmail.com>
2018-10-09 14:15:54 -0500

    Agregando ejemplos en el controlador y la vista del Login

--- Primera Iteración/Proyecto/biosip/src/main/java/mx/unam/is20191/controller/LoginController.java
+++ Primera Iteración/Proyecto/biosip/src/main/java/mx/unam/is20191/controller/LoginController.java
@@ -6,31 +6,40 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import mx.unam.is20191.dao.UsuarioDao;
-import mx.unam.is20191.models.Usuario;
 
 @ManagedBean
 @RequestScoped
 public class LoginController {
 
-    private Usuario usuario = new Usuario();
+    private String userName, password;
 
-    private final UsuarioDao USUARIO_DAO = new UsuarioDao();
+    private final UsuarioDao USUARIO_DAO;
 
-    public Usuario getUsuario() {
-        return usuario;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public LoginController() {
         FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale("es-Mx"));
+        USUARIO_DAO = new UsuarioDao();
     }
 
     public String loginUser() {
+        this.USUARIO_DAO.searchByUserNameOrEmail("user1");
         FacesContext.getCurrentInstance().addMessage(null,
-                new FacesMessage(FacesMessage.SEVERITY_ERROR, "El usuario y/o ´la contraseña son inválidos.", ""));
+                new FacesMessage(FacesMessage.SEVERITY_ERROR, "El usuario y/o la contraseña son inválidos.", ""));
         return null;
     }
 
