11625b015f592e4aefaef2bfdd15e30ce589433f
dams_ <dams_@LAPTOP-GRQR4L5Q>
2018-10-09 16:34:29 -0500

    Modifiqué la barra de navegación integrnado el botón "Menú", modificando el estilo la barra de busqueda y el botón de cerrar sesión.
    Cree una carpeta para los scripts.
    
    Cree el archivo xhtml para crear una categoría 
    y se hizo un primer boceto del archivo reservarmaterial

---
+++ Primera Iteración/Proyecto/biosip/src/main/webapp/scripts/template.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var open = false;
+function openNav() {
+    if (open) {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        open = false;
+    } else {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        open = true;
+    }
+}
+
