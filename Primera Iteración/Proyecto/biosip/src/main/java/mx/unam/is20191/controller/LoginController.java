96e4d722fae6b662f2f978b9ca06c97256013e61
Josué Cárdenas <rodrigo.cardns@gmail.com>
2018-10-12 13:10:39 -0500

    Agregando y probando nuevos mecanismos de validación en el registro y actualizando unos detalles en el login.

--- Primera Iteración/Proyecto/biosip/src/main/java/mx/unam/is20191/controller/LoginController.java
+++ Primera Iteración/Proyecto/biosip/src/main/java/mx/unam/is20191/controller/LoginController.java
@@ -39,14 +39,14 @@ public class LoginController {
     }
 
     public String loginUser() {
-        try {
-            Mail.mandarLinkDeRegistro("rodrigo.cardns@ciencias.unam.mx");
+        /*try {
+            //Mail.mandarLinkDeRegistro("rodrigo.cardns@ciencias.unam.mx");
         } catch (MessagingException ex) {
             FacesContext.getCurrentInstance().addMessage("messages",
                     new FacesMessage(FacesMessage.SEVERITY_ERROR, "Por el momento no se pueden registrar nuevas cuentas. Inténtalo más tarde.", ""));
             System.err.println(ex);
             return null;
-        }
+        }*/
         this.USUARIO_DAO.searchByUserNameOrEmail("user1");
         FacesContext.getCurrentInstance().addMessage("messages",
                 new FacesMessage(FacesMessage.SEVERITY_ERROR, "El usuario y/o la contraseña son inválidos.", ""));
--- Primera Iteración/Proyecto/biosip/src/main/java/mx/unam/is20191/controller/RegistroController.java
+++ Primera Iteración/Proyecto/biosip/src/main/java/mx/unam/is20191/controller/RegistroController.java
@@ -4,16 +4,17 @@ import java.util.Locale;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
-import javax.mail.MessagingException;
+import javax.faces.validator.ValidatorException;
 import mx.unam.is20191.dao.UsuarioDao;
-import mx.unam.is20191.utils.Mail;
 
 @ManagedBean
 @RequestScoped
 public class RegistroController {
 
-    private String userName, password;
+    private String userName, password, password2;
 
     private final UsuarioDao USUARIO_DAO;
 
@@ -39,18 +40,32 @@ public class RegistroController {
     }
 
     public String loginUser() {
-        try {
+        /*try {
             Mail.mandarLinkDeRegistro("rodrigo.cardns@ciencias.unam.mx");
         } catch (MessagingException ex) {
             FacesContext.getCurrentInstance().addMessage("messages",
                     new FacesMessage(FacesMessage.SEVERITY_ERROR, "Por el momento no se pueden registrar nuevas cuentas. Inténtalo más tarde.", ""));
             System.err.println(ex);
             return null;
-        }
+        }*/
         this.USUARIO_DAO.searchByUserNameOrEmail("user1");
         FacesContext.getCurrentInstance().addMessage("messages",
                 new FacesMessage(FacesMessage.SEVERITY_ERROR, "El usuario y/o la contraseña son inválidos.", ""));
         return null;
+    }
+
+    public void validatePassword(FacesContext context, UIComponent component,
+            Object value) {
+        System.err.println("KKKKKKKKKKKKKKKKKKKKKKKKKK");
+        // Retrieve the value passed to this method
+        String confirmPassword = (String) value;
+        // Retrieve the temporary value from the password field
+        UIInput passwordInput = (UIInput) component.findComponent("password");
+        String password = (String) passwordInput.getLocalValue();
+        if (password == null || confirmPassword == null || !password.equals(confirmPassword)) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Las contraseñas no coinciden", "Las contraseñas no coinciden");
+            throw new ValidatorException(msg);
+        }
     }
 
 }
