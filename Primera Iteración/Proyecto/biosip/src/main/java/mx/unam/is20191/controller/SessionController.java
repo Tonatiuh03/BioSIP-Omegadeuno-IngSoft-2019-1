c25239f64ec951e403b8ad8c489365ee93d9398b
dams_ <dams_@LAPTOP-GRQR4L5Q>
2018-10-19 21:16:05 -0500

    Caso de uso Crear Categoría terminado. Despligue de Material en la página Reservar Material. Modificación del botón Cerrar Sesión, se fijo dentro de la barra negra del Header. Modificación del color de letras del footer.

--- Primera Iteración/Proyecto/biosip/src/main/java/mx/unam/is20191/controller/SessionController.java
+++ Primera Iteración/Proyecto/biosip/src/main/java/mx/unam/is20191/controller/SessionController.java
@@ -5,22 +5,74 @@
  */
 package mx.unam.is20191.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import mx.unam.is20191.dao.UsuarioDao;
 import mx.unam.is20191.models.Usuario;
-import mx.unam.is20191.utils.Config;
 import static mx.unam.is20191.utils.Config.IMG_PROFILE_REPO;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 
 /**
+ * Clase que implementa operaciones útiles con la sesión.
  *
- * @author sds
+ * @author Josué Cárdenas
  */
 @ManagedBean
 @SessionScoped
 public class SessionController {
 
-    public String getImageUrl() {
-        return IMG_PROFILE_REPO + ((Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("user")).getRutaImagen();
+    /**
+     * Objeto en donde tendremos presente la información del usuario.
+     */
+    private Usuario usuario;
+
+    /**
+     * Método que obtiene la imagen de perfil del usuario.
+     *
+     * @return La imagen de perfil del usuario .
+     */
+    public StreamedContent getImage() {
+        try {
+            String profilePicturePath = IMG_PROFILE_REPO + getUsuario().getRutaImagen();
+            return new DefaultStreamedContent(new FileInputStream(new File(profilePicturePath)), "png");
+        } catch (IOException ex) {
+            return null;
+        }
+    }
+
+    /**
+     * Método que obtiene al usuario que está loggeado.
+     *
+     * @return El usuario que está logueado.
+     */
+    public Usuario getUsuario() {
+        if (usuario == null) {
+            usuario = (Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("user");
+        }
+        return usuario;
+    }
+
+    /**
+     * Método para facilitar la validación de que si el usuario es
+     * administrador.
+     *
+     * @return True si es administrador, falso en otro caso.
+     */
+    public boolean isAdmin() {
+        return UsuarioDao.isAdmin(getUsuario());
+    }
+
+    /**
+     * Método que facilita la validación de si el usuario es profesor.
+     *
+     * @return True si es profesor, falso en otro caso.
+     */
+    public boolean isProfesor() {
+        return UsuarioDao.isProfesor(getUsuario());
     }
 }
--- Primera Iteración/Proyecto/biosip/src/main/webapp/scripts/template.js
+++ Primera Iteración/Proyecto/biosip/src/main/webapp/scripts/template.js
@@ -7,10 +7,12 @@
 var open = false;
 function openNav() {
     if (open) {
+        $(".profile-picture").show();
         document.getElementById("mySidenav").style.width = "0";
         document.getElementById("main").style.marginLeft = "0";
         open = false;
     } else {
+        $(".profile-picture").hide();
         document.getElementById("mySidenav").style.width = "250px";
         document.getElementById("main").style.marginLeft = "250px";
         open = true;
